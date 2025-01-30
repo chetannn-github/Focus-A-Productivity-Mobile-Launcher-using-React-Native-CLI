be17fa5d6f4fe7966cca39fd134a424871551a3c
chetannn-github <chetan.rajawat25@gmail.com>
2025-01-30 20:42:05 +0530

    feat: update HomeScreen to use MaterialIcons and improve phone link functionality.

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { View, Linking } from "react-native";
 import Progress from "../Components/Progress";
 import { useNavigation } from "@react-navigation/native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
 
 export function HomeScreen() {
     const navigation = useNavigation();
@@ -14,8 +16,9 @@ export function HomeScreen() {
         <View style={{ flex: 1, backgroundColor: 'black', padding: 25 }}>
             <Progress />
             <View style={{ position: 'absolute', bottom: 20, left: 0, right: 0, flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20 }}>
-                <Icon name="ellipsis-h" size={30} color="white" onPress={() => navigation.openDrawer()} />
+                <Icon name="more-horiz" size={30} color="white" onPress={() => navigation.openDrawer()} />
                 <Icon name="phone" size={30} color="white" onPress={openPhoneApp} />
+                
             </View>
         </View>
     );
