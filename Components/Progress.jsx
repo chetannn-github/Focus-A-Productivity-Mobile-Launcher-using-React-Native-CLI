bed2253090cdb8ebdbbdd79d463dbc266866dccd
chetannn-github <chetan.rajawat25@gmail.com>
2025-02-02 09:12:58 +0530

    feat: add pull-to-refresh functionality to update.

--- Components/Progress.jsx
+++ Components/Progress.jsx
@@ -1,12 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useEffect, useState, useCallback } from 'react';
+import { View, Text, StyleSheet, ScrollView, RefreshControl } from 'react-native';
 import { ProgressBar } from 'react-native-paper';
 import { SettingsContext } from '../Context/SettingsContext';
-// Import stylesheet
 
 const Progress = () => {
   const { leetcodeUsername } = useContext(SettingsContext);
   const [leetcodeData, setLeetcodeData] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
 
   const fetchLeetcodeAPI = async () => {
     try {
@@ -24,48 +24,53 @@ const Progress = () => {
     }
   }, [leetcodeUsername]);
 
-  if(leetcodeUsername === '' || leetcodeUsername === null) {
-    return;
-  }
-  // Agar data abhi tak nahi aaya toh loading dikhayenge
-  if (!leetcodeData) {
-    return (
-      <View style={styles.progressContainer}>
-        <Text style={styles.text}>Loading...</Text>
-      </View>
-    );
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await fetchLeetcodeAPI();
+    setRefreshing(false);
+  }, []);
+
+  if (!leetcodeUsername || !leetcodeData) {
+    return null;
   }
 
-  // Progress Calculation (Round Off)
+  
+
   const easyProgress = leetcodeData.totalEasy ? Math.round((leetcodeData.easySolved / leetcodeData.totalEasy) * 100) : 0;
   const mediumProgress = leetcodeData.totalMedium ? Math.round((leetcodeData.mediumSolved / leetcodeData.totalMedium) * 100) : 0;
   const hardProgress = leetcodeData.totalHard ? Math.round((leetcodeData.hardSolved / leetcodeData.totalHard) * 100) : 0;
   const rank = Number(leetcodeData.ranking).toLocaleString('en-US');
+
   return (
-    <View style={styles.progressContainer}>
-      <Text style={styles.text}>Leetcode Rank: {rank}</Text>
+    <ScrollView
+      contentContainerStyle={styles.scrollView}
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+    >
+      <View style={styles.progressContainer}>
+        <View style= {styles.innerContainer}>
+          <Text style={styles.text}>Leetcode Rank: {rank}</Text>
 
-      <Text style={styles.text}>Easy: {leetcodeData.easySolved} / {leetcodeData.totalEasy}</Text>
-      <ProgressBar progress={easyProgress / 100} color="white" style={styles.progressBar} />
+          <Text style={styles.text}>Easy: {leetcodeData.easySolved} / {leetcodeData.totalEasy}</Text>
+          <ProgressBar progress={easyProgress / 100} color="white" style={styles.progressBar} />
 
-      <Text style={styles.text}>Medium: {leetcodeData.mediumSolved} / {leetcodeData.totalMedium}</Text>
-      <ProgressBar progress={mediumProgress / 100} color="white" style={styles.progressBar} />
+          <Text style={styles.text}>Medium: {leetcodeData.mediumSolved} / {leetcodeData.totalMedium}</Text>
+          <ProgressBar progress={mediumProgress / 100} color="white" style={styles.progressBar} />
 
-      <Text style={styles.text}>Hard: {leetcodeData.hardSolved} / {leetcodeData.totalHard}</Text>
-      <ProgressBar progress={hardProgress / 100} color="white" style={styles.progressBar} />
-    </View>
+          <Text style={styles.text}>Hard: {leetcodeData.hardSolved} / {leetcodeData.totalHard}</Text>
+          <ProgressBar progress={hardProgress / 100} color="white" style={styles.progressBar} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
-
-
-
-
 const styles = StyleSheet.create({
   progressContainer: {
+    width: '100%',
+    // padding: 5,
+  },
+  innerContainer:{
     width: '47%',
-    // backgroundColor: 'blue',
-    padding:5,
   },
   progressBar: {
     height: 9,
@@ -76,6 +81,9 @@ const styles = StyleSheet.create({
     fontSize: 13,
     color: 'white',
     marginBottom: 6,
+  },
+  scrollView: {
+    flexGrow: 1,
   },
 });
 
